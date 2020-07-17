817e9a80ccc99a096ccebc21f91eeaa0b4d318ef
yfbsei <61809667+yfbsei@users.noreply.github.com>
2020-07-17 18:49:50 -0400

    Update Autokick-ban-3.js

--- Autokick-ban-3.js
+++ Autokick-ban-3.js
@@ -7,7 +7,7 @@ var x = document.getElementsByClassName('message__avatar')
 
 getId = () => {
 for (i = 0; i < x.length; i++) {
-  if (x[i].naturalWidth < 99) {
+  if (x[i].naturalWidth < 60) {
     const pu = x[i].src.substr(41, 36)
     ava.push(pu);
   }
