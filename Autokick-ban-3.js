d3112b1d6f9fdce253e3df71ebefd7fdfd2a1fe2
yfbsei <61809667+yfbsei@users.noreply.github.com>
2020-07-17 14:38:35 -0400

    Update Autokick-ban-3.js

--- Autokick-ban-3.js
+++ Autokick-ban-3.js
@@ -1,32 +1,25 @@
+"use strict";
 //---Dom Mutaion| 
-var targetNode = document.querySelector('.chat-box-layer__messages')
-
+var targetNode = document.querySelector('.chat-box-layer__messages');
 var config = {
-  attributes: true,
-  childList: true,
-  subtree: false
+    attributes: true,
+    childList: true,
+    subtree: false
 };
-
-
-const callback = function(mutationsList) {
-  for (let mutation of mutationsList) {
-    if (mutation.addedNodes.length) {
-
-      //Gets Height+Width of user Avatar
-      const userAvatar = mutation.addedNodes[0].firstElementChild
-      const naturalHeight = userAvatar.naturalHeight
-      const naturalWidth = userAvatar.naturalWidth
-
-      if (naturalWidth < 99 && naturalHeight < 99) {
-        const kick = userAvatar.src.substr(41, 36);
-        CometdModerator.kickAccount(kick)
-        CometdModerator.removeAccountMessages(kick)
-      }
+const callback = function (mutationsList) {
+    for (let mutation of mutationsList) {
+        if (mutation.addedNodes.length) {
+            //Gets Height+Width of user Avatar
+            const userAvatar = mutation.addedNodes[0].firstElementChild;
+            const naturalHeight = userAvatar.naturalHeight;
+            const naturalWidth = userAvatar.naturalWidth;
+            if (naturalWidth < 99 && naturalHeight < 99) {
+                const kick = userAvatar.src.substr(41, 36);
+                CometdModerator.kickAccount(kick);
+                CometdModerator.removeAccountMessages(kick);
+            }
+        }
     }
-  }
 };
-
-
 const observer = new MutationObserver(callback);
-
 observer.observe(targetNode, config);
