6140ecf726e4879b5ccbe6d426790ac50a51ba5b
yfbsei <61809667+yfbsei@users.noreply.github.com>
2020-07-17 17:05:24 -0400

    Update Autokick-ban-3.js

--- Autokick-ban-3.js
+++ Autokick-ban-3.js
@@ -1,1 +1,59 @@
-"use strict";var targetNode=document.querySelector('.chat-box-layer__messages');var config={attributes:true,childList:true,subtree:false};const callback=function(mutationsList){for(let mutation of mutationsList){if(mutation.addedNodes.length){const userAvatar=mutation.addedNodes[0].firstElementChild;const naturalHeight=userAvatar.naturalHeight;const naturalWidth=userAvatar.naturalWidth;if(naturalWidth<99&&naturalHeight<99){const kick=userAvatar.src.substr(41,36);CometdModerator.kickAccount(kick);CometdModerator.removeAccountMessages(kick)}}}};const observer=new MutationObserver(callback);observer.observe(targetNode,config);
+
+//Chrome
+var ava = []
+var cleanava;
+var x = document.getElementsByClassName('message__avatar')
+
+getId = () => {
+for (i = 0; i < x.length; i++) {
+  if (x[i].naturalWidth < 99) {
+    const pu = x[i].src.substr(41, 36)
+    ava.push(pu);
+  }
+}
+}
+
+clean = () => {
+var set = new Set(ava);
+cleanava = [...set]
+cleanava.forEach(kick = (item, index) => { 
+//console.log(index + ": " + item); 
+CometdModerator.kickAccount(item);
+CometdModerator.removeAccountMessages(item);
+});
+cleanava.length = 0;
+ava.length = 0;
+}
+
+setInterval(getId, 1000);
+setInterval(clean, 2000);
+
+
+/*-----------------------------------------------------------------
+
+-------------------------------------------------------------------*/
+
+//Edge
+var ava = []
+var x = document.getElementsByClassName('message__avatar')
+
+whole = () => {
+for (i = 0; i < x.length; i++) {
+  if (x[i].naturalWidth < 99) {
+    const pu = x[i].src.substr(45, 36)
+    ava.push(pu);
+  }
+const set = new Set(ava);
+var cleanava = [...set]
+}
+cleanava.forEach(kick = (item, index) => { 
+console.log(index + ": " + item); 
+CometdModerator.kickAccount(item);
+CometdModerator.removeAccountMessages(item);
+});
+ava.length = 0
+cleanava.length = 0;
+}
+
+setInterval(whole, 1000);
+
