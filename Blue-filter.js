0b46b8f20b94244bb7e9d2586d40a82c55611a1c
yfbsei <61809667+yfbsei@users.noreply.github.com>
2020-08-14 18:29:31 -0400

    Delete Blue-filter.js

--- Blue-filter.js
+++
@@ -1,2 +0,0 @@
-setInterval( x = () => {const img = new Set(Array.from(document.querySelectorAll(".message__avatar")).filter(check => check.naturalHeight < 51).map(id => id.src.substring(41, 77)));
-const kick = Array.from(img).forEach((x, i) => {setTimeout(() => {CometdModerator.banAccount(x), CometdModerator.removeAccountMessages(x)}, i * 1000)}); img.clear(), RoomController.refresh()}, 3000);
