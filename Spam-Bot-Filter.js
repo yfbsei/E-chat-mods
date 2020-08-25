41b578c1d9d6bafd507bc2fbd9f89ad60e5cafcc
yfbsei <61809667+yfbsei@users.noreply.github.com>
2020-08-25 13:11:11 -0400

    Update Spam-Bot-Filter.js

--- Spam-Bot-Filter.js
+++ Spam-Bot-Filter.js
@@ -2,12 +2,13 @@ let count = 0;
 let spamBanned = 0;
 let guestBanned = 0;
 const messages = [];
-const wordList = ['Pm', 'pm', 'PM','mom','MOM','talk to me', 'https', 'http', 'Dm', 'Skype', 'x︍︍︍x︍︍ld︍︍︍a︍︍te︍︍︍.c︍︍︍o︍︍︍︍︍︍m', '.cc', 'app.unsee', 'inch', 'INCH', 'horny', 'BBC', 'message', 'snap', 'snapchat', 'discord', '.com', 'insta', 'instagram', '.site', '.SITE', '.︍︍︍︍︍p︍︍︍︍︍︍w', '.co', '.ca', 'www.', 'W︍︍W︍︍W︍︍︍︍.', 'snepchat', 'nudez', 'nudes', 'S`napchat', 'cam', 'cock', '.club', 'porn', 'Snapchat', 'sex chat', 'Any females here', 'any indian girl', 'chat'];
+let result;
+const wordList = [/\bPm\b/g, /\txt\b/g, /\bpm\b/g, /\bcum\b/g, /\bPM\b/g, /\bhmu\b/g, /\binvite=\b/g, /\btalk to me\b/g, /\bhttps\b/g, /\bhttp\b/g, /\bDm\b/g, /\bDM\b/g, /\bdm\b/g, /\bSkype\b/g, /\bskype\b/g, /\bx︍︍︍x︍︍ld︍︍︍a︍︍te︍︍︍.c︍︍︍o︍︍︍︍︍︍m\b/g, /\b.cc\b/g, /\bapp.unsee\b/g, /\binch\b/g, /\bINCH\b/g, /\bhorny\b/g, /\bBBC\b/g, /\bmessage\b/g, /\bsnap\b/g, /\bsnapchat\b/g, /\bdiscord\b/g, /\b.com\b/g, /\binsta\b/g, /\binstagram\b/g, /\b.site\b/g, /\b.SITE\b/g, /\b.︍︍︍︍︍p︍︍︍︍︍︍w\b/g, /\b.co\b/g, /\b.ca\b/g, /\bwww.\b/g, /\bW︍︍W︍︍W︍︍︍︍.\b/g, /\bsnepchat\b/g, /\bAny girls\b/g, /\bnudez\b/g, /\bnudes\b/g, /\bS`napchat\b/g, /\bcam\b/g, /\bcock\b/g, /\b.club\b/g, /\bporn\b/g, /\bSnapchat\b/g, /\bsex chat\b/g, /\bAny females here\b/g, /\bany indian girl\b/g, /\bprivate\b/];
 ban = id => {
     CometdConversation.open(id), CometdConversation.sendMessage('Banned for 10min: Flagged for potentially spamming', id), CometdConversation.close(id), CometdModerator.banAccount(id), CometdModerator.removeAccountMessages(id), count = 0, messages.length = 0, spamBanned = spamBanned + 1;
 };
 banGuest = (id, message) => {
-    CometdConversation.open(id), CometdConversation.sendMessage('Banned for 10min: Flagged Due to posting link, inappropriate word or social media advertising. Sign up not to get banned next time!. Message which was flagged: ', id), CometdConversation.sendMessage(message, id), CometdConversation.close(id), CometdModerator.banAccount(id), CometdModerator.removeAccountMessages(id), count = 0, guestBanned = guestBanned + 1;
+    CometdConversation.open(id), CometdConversation.sendMessage('Banned for 10min: Flagged Due to either posting: inappropriate words, social media, links, or asking to chat in private. Sign up not to get banned next time!. Message which was flagged:', id), CometdConversation.sendMessage(message, id), CometdConversation.close(id), CometdModerator.banAccount(id), CometdModerator.removeAccountMessages(id), count = 0, guestBanned = guestBanned + 1;
 };
 window.setInterval(unBan = () => {
     Array.from(document.querySelectorAll(".ModeratorPanelBannedUserAvatar")).filter(check => check.naturalHeight > 49).map(id => id.src.substring(41, 77)).forEach(y => CometdModerator.unbanAccount(y))
@@ -39,9 +40,10 @@ const callback = function(mutationsList) {
                 ban(userAvatar.src.substring(41, 77))
             };
 
-            userAvatar.naturalHeight < 51 && wordList.some(word => userMessage.includes(word)) ? banGuest(userAvatar.src.substring(41, 77), userMessage) : false;
+            wordList.some(v => userMessage === v) ? console.log('found') : console.log('false')
+            userAvatar.naturalHeight < 51 && wordList.some(rx => rx.test(userMessage)) ? banGuest(userAvatar.src.substring(41, 77), userMessage) : false;
         }
-    }
+    } 
 };
 
 const observer = new MutationObserver(callback);
